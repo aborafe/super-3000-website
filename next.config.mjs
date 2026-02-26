0805f70c8424e804dce12147749d960e81de8b8d
aborafe <105521715+aborafe@users.noreply.github.com>
2026-02-27 00:50:23 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,19 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig = {
   reactStrictMode: true,
+
+  // مهم جداً لـ GitHub Pages
+  output: "export",
+
+  // عشان الصور تشتغل بدون سيرفر
   images: {
+    unoptimized: true,
     formats: ['image/avif', 'image/webp']
-  }
+  },
+
+  // اسم الريبو عندك
+  basePath: "/super-3000-website",
+  assetPrefix: "/super-3000-website/"
 };
 
 export default withNextIntl(nextConfig);
